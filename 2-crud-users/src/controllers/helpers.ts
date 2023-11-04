f8998ae36aa5c419a373b70d99fe489ca948fde1
Leandro Campos <camposleandro748@gmail.com>
2023-11-04 11:42:01 -0300

    2-crud-users | feat: created protocols and helpers to http request and response

---
+++ 2-crud-users/src/controllers/helpers.ts
@@ -0,0 +1,21 @@
+import { HttpResponse, HttpStatusCode } from './protocols';
+
+export const ok = <T>(body: T): HttpResponse<T> => ({
+  statusCode: HttpStatusCode.OK,
+  body,
+});
+
+export const created = <T>(body: T): HttpResponse<T> => ({
+  statusCode: HttpStatusCode.CREATED,
+  body,
+});
+
+export const badRequest = (body: string): HttpResponse<string> => ({
+  statusCode: HttpStatusCode.BAD_REQUEST,
+  body,
+});
+
+export const serverError = (): HttpResponse<string> => ({
+  statusCode: HttpStatusCode.SERVER_ERROR,
+  body: 'Something went wrong',
+});
