6818f6094b306998b48886d73ea04b49c4befd91
Leandro Campos <camposleandro748@gmail.com>
2023-11-04 11:37:59 -0300

    2-crud-users | feat: created connection to mongodb database

---
+++ 2-crud-users/src/database/mongo.ts
@@ -0,0 +1,26 @@
+import { MongoClient as Mongo, Db, WithId } from 'mongodb';
+import { MongoUser } from '../repositories/mongo-protocols';
+import { User } from '../models/user';
+
+export const MongoClient = {
+  client: undefined as unknown as Mongo,
+  db: undefined as unknown as Db,
+
+  async connect(): Promise<void> {
+    const url = process.env.MONGODB_URL || 'mongodb://localhost:27017';
+    const username = process.env.MONGODB_USERNAME;
+    const password = process.env.MONGODB_PASSWORD;
+
+    this.client = new Mongo(url, { auth: { username, password } });
+    this.db = this.client.db('crud-users');
+
+    console.log('Connected to mongodb!');
+  },
+
+  mapMongoUserToUser(users: WithId<MongoUser>[]): User[] {
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
+  },
+};
