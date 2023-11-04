f1e5137f92f59f4c88f47d66285eeea539dea887
Leandro Campos <camposleandro748@gmail.com>
2023-11-04 11:40:44 -0300

    2-crud-users | feat: created MongoGetUsersRepository

---
+++ 2-crud-users/src/repositories/get-users/mongo-get-users.ts
@@ -0,0 +1,15 @@
+import { IGetUsersRepository } from '../../controllers/get-users/protocols';
+import { MongoClient } from '../../database/mongo';
+import { User } from '../../models/user';
+import { MongoUser } from '../mongo-protocols';
+
+export class MongoGetUsersRepository implements IGetUsersRepository {
+  async getUsers(): Promise<User[]> {
+    const users = await MongoClient.db
+      .collection<MongoUser>('users')
+      .find({})
+      .toArray();
+
+    return MongoClient.mapMongoUserToUser(users);
+  }
+}
