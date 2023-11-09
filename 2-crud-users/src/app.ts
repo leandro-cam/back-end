aaaed270a2a70eaa188972f7d7ed98066becea4a
Leandro Campos <camposleandro748@gmail.com>
2023-11-09 05:20:12 -0300

    2-crud-users | refactor: main file now is app.ts, deleted index.ts, routes in separated file

---
+++ 2-crud-users/src/app.ts
@@ -0,0 +1,39 @@
+import { config } from 'dotenv';
+import express from 'express';
+import { MongoClient } from './database/mongo';
+import userRoutes from './routes/user-routes';
+
+class App {
+  private app = express();
+
+  constructor() {
+    config();
+
+    this.database();
+
+    this.use();
+    this.routes();
+    this.listen();
+  }
+
+  async database() {
+    await MongoClient.connect();
+  }
+
+  use() {
+    // this.app.use(express.json());
+    // this.app.use(cors(corsOptions));
+    // this.app.use(helmet());
+  }
+
+  routes() {
+    this.app.use('/', userRoutes);
+  }
+
+  listen() {
+    const port = process.env.PORT;
+    this.app.listen(port, () => console.log(`Listening on port ${port}!`));
+  }
+}
+
+new App();
---
+++ 2-crud-users/src/routes/user-routes.ts
@@ -0,0 +1,86 @@
+import { Router } from 'express';
+import { CreateUserController } from '../controllers/create-user/create-user';
+import { DeleteUserController } from '../controllers/delete-user/delete-user';
+import { GetUserByIdController } from '../controllers/get-user-by-id/get-user-by-id';
+import { GetUsersController } from '../controllers/get-users/get-users';
+import { UpdateUserAllFieldsController } from '../controllers/update-user/update-user-all-fields';
+import { UpdateUserSomeFieldController } from '../controllers/update-user/update-user-some-field';
+import { MongoCreateUserRepository } from '../repositories/create-user/mongo-create-user';
+import { MongoDeleteUserRepository } from '../repositories/delete-user/mongo-delete-user';
+import { MongoGetUserByIdRepository } from '../repositories/get-user-by-id/mongo-get-user-by-id';
+import { MongoGetUsersRepository } from '../repositories/get-users/mongo-get-users';
+import { MongoUpdateUserRepository } from '../repositories/update-user/mongo-update-user';
+
+const router = Router();
+
+router.get('/users', async (_, res) => {
+  const mongoGetUsersRepository = new MongoGetUsersRepository();
+  const getUsersController = new GetUsersController(mongoGetUsersRepository);
+  const { statusCode, body } = await getUsersController.handle();
+
+  res.status(statusCode).send(body);
+});
+
+router.get('/users/:userId', async (req, res) => {
+  const mongoGetUserByIdRepository = new MongoGetUserByIdRepository();
+  const getUserByIdController = new GetUserByIdController(
+    mongoGetUserByIdRepository,
+  );
+  const { statusCode, body } = await getUserByIdController.handle({
+    params: req.params,
+  });
+
+  res.status(statusCode).send(body);
+});
+
+router.post('/users', async (req, res) => {
+  const mongoCreateUserRepository = new MongoCreateUserRepository();
+  const createUserController = new CreateUserController(
+    mongoCreateUserRepository,
+  );
+  const { statusCode, body } = await createUserController.handle({
+    body: req.body,
+  });
+
+  res.status(statusCode).send(body);
+});
+
+router.delete('/users/:userId', async (req, res) => {
+  const mongoDeleteUserRepository = new MongoDeleteUserRepository();
+  const deleteUserController = new DeleteUserController(
+    mongoDeleteUserRepository,
+  );
+  const { statusCode, body } = await deleteUserController.handle({
+    params: req.params,
+  });
+
+  res.status(statusCode).send(body);
+});
+
+router.patch('/users/:userId', async (req, res) => {
+  const mongoUpdateUserRepository = new MongoUpdateUserRepository();
+  const updateUserSomeFieldController = new UpdateUserSomeFieldController(
+    mongoUpdateUserRepository,
+  );
+  const { statusCode, body } = await updateUserSomeFieldController.handle({
+    params: req.params,
+    body: req.body,
+  });
+
+  res.status(statusCode).send(body);
+});
+
+router.put('/users/:userId', async (req, res) => {
+  const mongoUpdateUserRepository = new MongoUpdateUserRepository();
+  const updateUserAllFieldsController = new UpdateUserAllFieldsController(
+    mongoUpdateUserRepository,
+  );
+  const { statusCode, body } = await updateUserAllFieldsController.handle({
+    params: req.params,
+    body: req.body,
+  });
+
+  res.status(statusCode).send(body);
+});
+
+export default router;
