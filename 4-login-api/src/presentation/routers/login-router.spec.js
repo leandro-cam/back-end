240da8549caa1ed098ae196f89718e93ea191ed9
Leandro Campos <camposleandro748@gmail.com>
2024-01-15 05:54:26 -0300

    fix: fixed test name (tested that password is not passed)

--- 4-login-api/src/presentation/routers/login-router.spec.js
+++ 4-login-api/src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route(httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.password) {
       return {
         statusCode: 400,
       };
@@ -9,9 +9,9 @@ class LoginRouter {
 }
 
 describe('Login Router', () => {
-  test('should return 400 if email is not passed', () => {
+  test('should return 400 if password is not passed', () => {
     const sut = new LoginRouter();
-    const httpRequest = { body: { password: 'any_password' } };
+    const httpRequest = { body: { email: 'any_email' } };
     const httpResponse = sut.route(httpRequest);
 
     expect(httpResponse.statusCode).toBe(400);
