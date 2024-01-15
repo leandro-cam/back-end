76a4afb9102353b8af5c2f5fd1742dd57c8f3385
Leandro Campos <camposleandro748@gmail.com>
2024-01-15 05:57:22 -0300

    feat: now yes, tested that LoginRouter returns 400 is email is not passed

--- 4-login-api/src/presentation/routers/login-router.spec.js
+++ 4-login-api/src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route(httpRequest) {
-    if (!httpRequest.body.password) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400,
       };
@@ -9,6 +9,14 @@ class LoginRouter {
 }
 
 describe('Login Router', () => {
+  test('should return 400 if email is not passed', () => {
+    const sut = new LoginRouter();
+    const httpRequest = { body: { password: 'any_password' } };
+    const httpResponse = sut.route(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+  });
+
   test('should return 400 if password is not passed', () => {
     const sut = new LoginRouter();
     const httpRequest = { body: { email: 'any_email' } };
