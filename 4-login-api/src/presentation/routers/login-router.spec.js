2169dbee4b0d629676a4d2640ce1e0d4093699bc
Leandro Campos <camposleandro748@gmail.com>
2024-01-17 21:39:04 -0300

    feat: tested that LoginRouter calls AuthUseCase with correct params

--- 4-login-api/src/presentation/routers/login-router.spec.js
+++ 4-login-api/src/presentation/routers/login-router.spec.js
@@ -2,12 +2,25 @@ const MissingParamError = require('../helpers/errors/missing-param-error');
 const LoginRouter = require('./login-router');
 
 const makeSut = () => {
-  return new LoginRouter();
+  class AuthUseCaseSpy {
+    auth(email, password) {
+      this.email = email;
+      this.password = password;
+    }
+  }
+
+  const authUseCaseSpy = new AuthUseCaseSpy();
+  const sut = new LoginRouter(authUseCaseSpy);
+
+  return {
+    authUseCaseSpy,
+    sut,
+  };
 };
 
 describe('Login Router', () => {
   test('should return 400 if email is not passed', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = { body: { password: 'any_password' } };
     const httpResponse = sut.route(httpRequest);
 
@@ -16,7 +29,7 @@ describe('Login Router', () => {
   });
 
   test('should return 400 if password is not passed', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = { body: { email: 'any_email' } };
     const httpResponse = sut.route(httpRequest);
 
@@ -25,16 +38,26 @@ describe('Login Router', () => {
   });
 
   test('should return 500 if httpRequest is not passed', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpResponse = sut.route();
 
     expect(httpResponse.statusCode).toBe(500);
   });
 
   test('should return 500 if httpRequest has no body', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpResponse = sut.route({});
 
     expect(httpResponse.statusCode).toBe(500);
+  });
+
+  test('should call AuthUseCase with correct params', () => {
+    const { authUseCaseSpy, sut } = makeSut();
+    const httpRequest = { body: { email: 'any_email', password: 'any_password' } };
+
+    sut.route(httpRequest);
+
+    expect(authUseCaseSpy.email).toBe(httpRequest.body.email);
+    expect(authUseCaseSpy.password).toBe(httpRequest.body.password);
   });
 });
