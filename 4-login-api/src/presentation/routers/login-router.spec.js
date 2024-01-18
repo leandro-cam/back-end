4e810fe5f5f874c26ce786748e482822b1bba0d6
Leandro Campos <camposleandro748@gmail.com>
2024-01-18 20:55:30 -0300

    feat: tested that LoginRouter returns 200 when valid credentials are passed

--- 4-login-api/src/presentation/routers/login-router.spec.js
+++ 4-login-api/src/presentation/routers/login-router.spec.js
@@ -7,10 +7,14 @@ const makeSut = () => {
     auth(email, password) {
       this.email = email;
       this.password = password;
+
+      return this.accessToken;
     }
   }
 
   const authUseCaseSpy = new AuthUseCaseSpy();
+  authUseCaseSpy.accessToken = 'valid_token';
+
   const sut = new LoginRouter(authUseCaseSpy);
 
   return {
@@ -79,11 +83,21 @@ describe('Login Router', () => {
   });
 
   test('should return 401 when invalid credentials are passed', () => {
-    const { sut } = makeSut();
+    const { authUseCaseSpy, sut } = makeSut();
+    authUseCaseSpy.accessToken = null;
+
     const httpRequest = { body: { email: 'invalid_email', password: 'invalid_password' } };
     const httpResponse = sut.route(httpRequest);
 
     expect(httpResponse.statusCode).toBe(401);
     expect(httpResponse.body).toEqual(new UnauthorizedError());
+  });
+
+  test('should return 200 when valid credentials are passed', () => {
+    const { sut } = makeSut();
+    const httpRequest = { body: { email: 'valid_email', password: 'valid_password' } };
+    const httpResponse = sut.route(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(200);
   });
 });
