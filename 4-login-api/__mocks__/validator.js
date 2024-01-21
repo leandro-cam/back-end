4531a7915b94827384f71bdeaa8d3bce36a274f2
Leandro Campos <camposleandro748@gmail.com>
2024-01-21 10:07:04 -0300

    feat: tested that EmailValidator returns false when validator returns false

---
+++ 4-login-api/__mocks__/validator.js
@@ -0,0 +1,8 @@
+module.exports = {
+  isValidEmail: true,
+
+  // eslint-disable-next-line no-unused-vars
+  isEmail(email) {
+    return this.isValidEmail;
+  },
+};
--- 4-login-api/src/utils/email-validator.spec.js
+++ 4-login-api/src/utils/email-validator.spec.js
@@ -1,14 +1,26 @@
+const validator = require('validator');
+
 class EmailValidator {
-  isValid() {
-    return true;
+  isValid(email, isValidEmail = true) {
+    validator.isValidEmail = isValidEmail;
+
+    return validator.isEmail(email);
   }
 }
 
 describe('EmailValidator', () => {
   test('should return true when validator returns true', () => {
     const sut = new EmailValidator();
+
     const isValidEmail = sut.isValid('valid_email');
 
     expect(isValidEmail).toBe(true);
+  });
+
+  test('should return false when validator returns false', () => {
+    const sut = new EmailValidator();
+    const isValidEmail = sut.isValid('invalid_email', false);
+
+    expect(isValidEmail).toBe(false);
   });
 });
