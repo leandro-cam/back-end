94a19c091d0a1e1a5f777386e932a60648cb563f
Leandro Campos <camposleandro748@gmail.com>
2024-06-16 21:43:43 -0300

    fix: test name

--- 4-login-api/src/domain/usecases/auth-usecase.spec.js
+++ 4-login-api/src/domain/usecases/auth-usecase.spec.js
@@ -7,7 +7,7 @@ class AuthUseCase {
 }
 
 describe('Auth UseCase', () => {
-  test('should return null if email is not provided', async () => {
+  test('should throw error if email is not provided', async () => {
     const sut = new AuthUseCase();
     const promise = sut.auth();
     expect(promise).rejects.toThrow();
