bfcf7b10138ae4fab964c7364b2fca3903bf2eba
Leandro Campos <camposleandro748@gmail.com>
2024-06-22 07:34:08 -0300

    feat: ensured that Encrypter throws Error if params are not provided

--- 4-login-api/src/utils/helpers/encrypter.spec.js
+++ 4-login-api/src/utils/helpers/encrypter.spec.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const Encrypter = require('./encrypter');
+const { MissingParamError } = require('../errors');
 
 const makeSut = () => {
   return new Encrypter();
@@ -31,5 +32,11 @@ describe('Encrypter', () => {
     await sut.compare('any_value', 'any_hash');
     expect(bcrypt.value).toBe('any_value');
     expect(bcrypt.hash).toBe('any_hash');
+  });
+
+  test('should throw Error if params are not provided', async () => {
+    const sut = makeSut();
+    expect(sut.compare()).rejects.toThrow(new MissingParamError('value'));
+    expect(sut.compare('any_value')).rejects.toThrow(new MissingParamError('hash'));
   });
 });
