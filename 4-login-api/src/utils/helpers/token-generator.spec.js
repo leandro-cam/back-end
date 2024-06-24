f2be7301ebd44afe716e69ecce1fcdaef3258b6c
Leandro Campos <camposleandro748@gmail.com>
2024-06-23 21:23:38 -0300

    refactor: moved TokenGenerator class to its own file

--- 4-login-api/src/utils/helpers/token-generator.spec.js
+++ 4-login-api/src/utils/helpers/token-generator.spec.js
@@ -1,23 +1,6 @@
 const jwt = require('jsonwebtoken');
 const { MissingParamError } = require('../errors');
-
-class TokenGenerator {
-  constructor(secret) {
-    this.secret = secret;
-  }
-
-  async generate(id) {
-    if (!id) {
-      throw new MissingParamError('id');
-    }
-
-    if (!this.secret) {
-      throw new MissingParamError('secret');
-    }
-
-    return jwt.sign(id, this.secret);
-  }
-}
+const TokenGenerator = require('./token-generator');
 
 const makeSut = () => {
   return new TokenGenerator('secret');
