4b3cdf9f6a812fae8ea4fac29989ed7dabaa75d8
Leandro Campos <camposleandro748@gmail.com>
2024-06-23 21:20:52 -0300

    feat: ensured that TokenGenerator throws Error if id is not provided

--- 4-login-api/src/utils/helpers/token-generator.spec.js
+++ 4-login-api/src/utils/helpers/token-generator.spec.js
@@ -7,6 +7,10 @@ class TokenGenerator {
   }
 
   async generate(id) {
+    if (!id) {
+      throw new MissingParamError('id');
+    }
+
     if (!this.secret) {
       throw new MissingParamError('secret');
     }
@@ -43,6 +47,12 @@ describe('TokenGenerator', () => {
     await sut.generate('any_id');
     expect(jwt.id).toBe('any_id');
     expect(jwt.secret).toBe(sut.secret);
+  });
+
+  test('should throw Error if id is not provided', async () => {
+    const sut = makeSut();
+    const promise = sut.generate();
+    expect(promise).rejects.toThrow(new MissingParamError('id'));
   });
 
   test('should throw Error if secret is not provided', async () => {
